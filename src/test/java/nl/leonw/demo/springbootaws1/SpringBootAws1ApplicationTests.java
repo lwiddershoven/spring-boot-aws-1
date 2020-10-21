5c7c71e51190b5e35a9c839ea268e780d0ae1205
Leon <qaz@dds.nl>
2020-10-21 16:16:24 +0200

    start.spring.io: Gneerated project with web and actuator dependencies

---
+++ src/test/java/nl/leonw/demo/springbootaws1/SpringBootAws1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.leonw.demo.springbootaws1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAws1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
