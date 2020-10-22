81082f0759df9f9794e89757989080b5128a39f6
Leon <qaz@dds.nl>
2020-10-22 22:03:40 +0200

    Start test with @ActiveProfiles("github"). If that works I need to remove all previous disable-aws commits. I hope I remember because cleaning is something I really want to forget :)

--- src/test/java/nl/leonw/demo/springbootaws1/SpringBootAws1ApplicationTests.java
+++ src/test/java/nl/leonw/demo/springbootaws1/SpringBootAws1ApplicationTests.java
@@ -1,9 +1,12 @@
 package nl.leonw.demo.springbootaws1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest()
+@ActiveProfiles("github")
 class SpringBootAws1ApplicationTests {
 
 	@Test
